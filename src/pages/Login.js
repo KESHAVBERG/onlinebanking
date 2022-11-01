7d56650d3b3d8ae7507cb86ab5ce057078086cfd
KESHAVBERG <kingkeshav7@gmail.com>
2022-11-01 11:35:31 +0530

    login-fields-done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,32 @@
 import React from "react";
+import '../css/formfiled.css'
+import { Box, Typography, TextField, Stack, FormControl } from "@mui/material";
+import {styled } from '@mui/material/styles';
 
-import { Box, Typography } from "@mui/material";
+const CssTextField = styled(TextField)({
+  width:'300px',
+  '& label.Mui-focused': {
+    color: 'green',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'green',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'white',
+    },
+    '&:hover fieldset': {
+      borderColor: 'yellow',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'green',
+    },
+  },
+});
+
 
 export const Login = () => {
+
   return (
     <Box
       sx={{ background: "linear-gradient(to right bottom, #36EAEF, #6B0AC9)" }}
@@ -12,7 +36,14 @@ export const Login = () => {
       minHeight="100vh"
       height="100vh"
     >
-      <Typography sx={{color:"white"}} fontSize="30px" fontWeight="bold">Online banking</Typography>
+      <Stack spacing={1}>
+        <Typography sx={{ color: "white", alignItems:"center", pl:{lg:"30px", md:"20px", xs:"10px"} }} fontSize="30px" fontWeight="bold">
+          Online banking
+        </Typography>
+        <CssTextField label="email" className="inputRounded" id="custom-css-outlined-input" />
+
+        <CssTextField label="password" className="inputRounded" id="custom-css-outlined-input" />
+      </Stack>
     </Box>
   );
 };
