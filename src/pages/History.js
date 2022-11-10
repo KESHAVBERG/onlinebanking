f22b1db5919ab9c8e1e03643fbedae5061973804
KESHAVBERG <kingkeshav7@gmail.com>
2022-11-11 05:26:25 +0530

    workign on fetching

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,29 +1,40 @@
 import { Box, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, collection } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
 
 export const History = () => {
-  const [currentUser, setCurrentUser] = useState("");
   const user = auth.currentUser;
 
-  console.log(user)
-  if (user !== null) {
-    const uid = user.uid;
-    console.log(uid)
-    setCurrentUser(uid)
-  }
-  
-  return (
-    <Box 
-    display="flex"
-    justifyContent="center"
-    alignItems="center"
-    minHeight="100vh"
-    height="100vh">
 
-            <Typography style={{fontWeight:"bold"}}>Your Transaction History</Typography>
+  async function getData() {
+    if (user !== null) {
+      const uid = user.uid;
+      const docRef = collection(db, "users", uid, "amount")
+      const docSnap = await getDoc(docRef)
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      height="100vh">
+
+      <Typography style={{ fontWeight: "bold" }}>Your Transaction History</Typography>
     </Box>
   )
 }
